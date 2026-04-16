f887c407282d3a1ba5caf893f8c7e2560b2e893c
ardy-priyo <priyo.ardy@hotmail.com>
2026-04-16 10:52:27 +0700

    fix: Fixed sidebar menu orderable

--- app/Filament/Resources/PaymentTerms/PaymentTermResource.php
+++ app/Filament/Resources/PaymentTerms/PaymentTermResource.php
@@ -13,6 +13,7 @@ use Filament\Resources\Resource;
 use Filament\Schemas\Schema;
 use Filament\Support\Icons\Heroicon;
 use Filament\Tables\Table;
+use UnitEnum;
 
 class PaymentTermResource extends Resource
 {
@@ -21,6 +22,9 @@ class PaymentTermResource extends Resource
     protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedRectangleStack;
 
     protected static ?string $recordTitleAttribute = 'PaymentTerm';
+    protected static string|UnitEnum|null $navigationGroup = 'Master Data';
+    protected static ?int $navigationSort = 10;
+    protected static ?string $pluralLabel = 'Payment Terms';
 
     public static function form(Schema $schema): Schema
     {
